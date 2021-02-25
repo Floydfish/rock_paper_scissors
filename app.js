84798f0a8bcb7b1615a09724915774e93116bb96
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-25 14:08:05 +0100

    Integrate weapon with the gameplay

--- app.js
+++ app.js
@@ -1,58 +1,70 @@
+const gameScore = { playerScore: 0, computerScore: 0 };
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const description = document.querySelector('.description p')
+
 function computerPlay() {
-    let x = Math.floor(Math.random() * (3))
-    switch (x) {
-    case 0: return "rock";
-    case 1: return "paper";
-    case 2: return "scissors";
-    }
+	let x = Math.floor(Math.random() * (3))
+	switch (x) {
+		case 0: return "rock";
+		case 1: return "paper";
+		case 2: return "scissors";
+	}
 }
 
-function playRound(playerSelection, computerSelection) {
-    let win = `You win! ${playerSelection} beats ${computerSelection}!`;;
-    let lose = `You lose! ${computerSelection}! beats ${playerSelection}!`;
-    let draw = `Draw! Both players chose ${playerSelection}`;
-    if (playerSelection === computerSelection) {
-        return draw;
-    } else {
-        switch(playerSelection) {
-            case ("rock"): 
-                if (computerSelection === "scissors") {
-                    gameScore.playerScore++
-                    return win;
-                } else {
-                    gameScore.computerScore++
-                    return lose;
-                }
-            case ("scissors"):
-                if (computerSelection === "paper") {
-                    gameScore.playerScore++
-                    return win;
-                } else {
-                    gameScore.computerScore++
-                    return lose;
-            }
-            case ("paper"):
-                if (computerSelection === "rock") {
-                    gameScore.playerScore++
-                    return win;
-                } else {
-                    gameScore.computerScore++
-                    return lose;
-            }
-        } 
-    }
+function playRound() {
+	computerSelection = computerPlay()
+	playerSelection = this.innerText.toLowerCase();
+	checkWin(playerSelection, computerSelection)
 }
 
-function game() {
-    for (i = 0; i <5; i++) {
-        let playerSelection = prompt("Please choose Paper, Rock or Scissors").toLowerCase();
-        while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors" ) {
-            playerSelection = prompt("Wrong choice, please choose Paper, Rock or Scissors").toLowerCase();
-        }
-        const computerSelection = computerPlay()
-        console.log(playRound(playerSelection, computerSelection))
-        console.log(gameScore)
-    }
+function checkWin(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
+		draw();
+	} else {
+		switch (playerSelection) {
+			case ("rock"):
+				if (computerSelection === "scissors") {
+					win();
+				} else {
+					lose();
+				}
+				break;
+			case ("scissors"):
+				if (computerSelection === "paper") {
+					win();
+				} else {
+					lose();
+				}
+				break;
+			case ("paper"):
+				if (computerSelection === "rock") {
+					win();
+				} else {
+					lose();
+				}
+				break;
+		}
+	}
 }
 
-let gameScore = {playerScore: 0, computerScore: 0}
+function win() {
+	gameScore.playerScore++;
+	playerScore.innerText = gameScore.playerScore
+	description.innerText = `${playerSelection} beats ${computerSelection}, YOU WIN!`
+	console.log(gameScore);
+}
+
+function lose() {
+	gameScore.computerScore++
+	computerScore.innerText = gameScore.computerScore
+	description.innerText = `${computerSelection} beats ${playerSelection}, you lose`
+	console.log(gameScore);
+}
+
+function draw() {
+	description.innerText = `You chose the same weapon, DRAW`
+}
+
+const weapons = document.querySelectorAll('.weapon')
+weapons.forEach(weapon => weapon.addEventListener('click', playRound))
